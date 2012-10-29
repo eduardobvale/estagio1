ec7a7a3daac2fdcb1471d3c321fc19ca4407a566
Eduardo Braz do Vale <edu.bvale@gmail.com>
2012-10-29 09:47:01 -0200

    Revert "Revert "Mudando a forma de renderizar os sprites""
    This reverts commit d353ba46a37f238e3d5ed05cb239d8fcc157bc94.

--- desenvolvimento/App/MyThirdApp.js
+++ desenvolvimento/App/MyThirdApp.js
@@ -31,15 +31,6 @@ var MyThirdApp = cc.Layer.extend(
         // Construct a world object, which will hold and simulate the rigid bodies.
         this.world = new b2World(new b2Vec2(0, -12), true);
         this.world.SetContinuousPhysics(true);
-
-        //setup debug draw
-        var debugDraw = new b2DebugDraw();
-        debugDraw.SetSprite(document.getElementById("gameCanvas2").getContext("2d"));
-        debugDraw.SetDrawScale(PTM_RATIO);
-        debugDraw.SetFillAlpha(0.8);
-        debugDraw.SetLineThickness(1.0);
-        debugDraw.SetFlags(b2DebugDraw.e_shapeBit | b2DebugDraw.e_jointBit);
-        this.world.SetDebugDraw(debugDraw);
 	
         var fixDef = new b2FixtureDef;
         fixDef.density = 1.0;
@@ -66,10 +57,7 @@ var MyThirdApp = cc.Layer.extend(
     init:function(){
 	this._super();    
 	
-        this._jetSprite.schedule(function()
-            {
-                this.setRotation(playerBody.GetLinearVelocity().y * -1);
-            });
+    
         return true;
     },
     addNewSpriteWithCoords:function (p) {
@@ -111,54 +99,6 @@ var MyThirdApp = cc.Layer.extend(
         playerBody.CreateFixture(fixtureDef);
 
     },
-    addNewSprite:function (vertices,position,upsidedown) {
-	
-	var p = position;
-	
-	var sprite = new GroundSprite();
-	//var radio = PTM_RATIO*side;
-	//var vertices = [cc.p(-radio, radio), cc.p(radio, PTM_RATIO*1),cc.p(radio, -radio),cc.p(-radio,-radio)];
-	var verticesInPixel = new Array();
-	for(var i in vertices){
-	    var vert = vertices[i];
-	    verticesInPixel.push(cc.p(vert.x*PTM_RATIO,vert.y*PTM_RATIO));
-	}
-	sprite.setVertices(verticesInPixel);
-        this.addChild(sprite);
-	
-	var height = Math.max(verticesInPixel[2].y,verticesInPixel[3].y);
-	if(upsidedown)
-	    p.y = p.y - height;
-	else
-	    p.y = p.y + height;
-        sprite.setPosition(cc.p(p.x, p.y));
-
-        
-        var b2BodyDef = Box2D.Dynamics.b2BodyDef
-            , b2Body = Box2D.Dynamics.b2Body
-            , b2FixtureDef = Box2D.Dynamics.b2FixtureDef
-            , b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape;
-
-        var bodyDef = new b2BodyDef();
-        bodyDef.type = b2Body.b2_staticBody;
-        bodyDef.position.Set(p.x / PTM_RATIO, p.y / PTM_RATIO);
-        bodyDef.userData = sprite;
-        var body = this.world.CreateBody(bodyDef);
-
-        var dynamicBox = new b2PolygonShape();
-	   dynamicBox.SetAsVector(vertices);
-
-        // Define the dynamic body fixture.
-        var fixtureDef = new b2FixtureDef();
-        fixtureDef.shape = dynamicBox;
-        fixtureDef.density = 10.0;
-        fixtureDef.friction = 0;
-	   //fixtureDef.isSensor = true;
-        body.CreateFixture(fixtureDef);
-	
-	this._groundElements.push(body);
-
-    },
     onEnter:function(){
         this._super();
     },
@@ -184,21 +124,6 @@ var MyThirdApp = cc.Layer.extend(
         // Instruct the world to perform a single step of simulation. It is
         // generally best to keep the time step and iterations fixed.
         this.world.Step(dt, velocityIterations, positionIterations);
-
-        
-
-
-        //Iterate over the bodies in the physics world
-        for (var b = this.world.GetBodyList(); b; b = b.GetNext()) {
-	
-            if (b.GetUserData() != null) {
-                //Synchronize the AtlasSprites position and rotation with the corresponding body
-                var myActor = b.GetUserData();
-                myActor.setPosition(cc.p(b.GetPosition().x * PTM_RATIO, b.GetPosition().y * PTM_RATIO));
-                //myActor.setRotation(-1 * cc.RADIANS_TO_DEGREES(b.GetAngle()));
-                //console.log(b.GetAngle());
-            }
-        }
 
 	
 	    playerBody.ApplyImpulse(cc.p(0,flyingVelocity),playerBody.GetPosition());
